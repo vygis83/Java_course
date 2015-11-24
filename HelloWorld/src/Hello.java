1f441115872d10293da432fea23a06d92459212c
vygis83 <vygis83@gmail.com>
2015-11-24 17:38:27 +0000

    Section 4

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,6 +3,14 @@
  */
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello, Vygis!");
+        int myNumber = 2 * 5;
+        int otherNumber = 24 / 2;
+        int myTotal = myNumber + otherNumber;
+        int newNum = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(newNum);
     }
 }
