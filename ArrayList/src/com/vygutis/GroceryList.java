2a643f3c721e37e0ce5cf8da6defa86134298aad
vygis83 <vygis83@gmail.com>
2015-12-08 14:58:56 +0000

    ArrayList

---
+++ ArrayList/src/com/vygutis/GroceryList.java
@@ -0,0 +1,31 @@
+package com.vygutis;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luksyvyg on 08/12/2015.
+ */
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your list");
+        for (int i=0; i<groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified");
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+}
