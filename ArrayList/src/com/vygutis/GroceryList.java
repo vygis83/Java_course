9a498c31883670eca9e1a34b1674da49238d2904
vygis83 <vygis83@gmail.com>
2015-12-21 14:40:00 +0000

    ArrayList2

--- ArrayList/src/com/vygutis/GroceryList.java
+++ ArrayList/src/com/vygutis/GroceryList.java
@@ -11,26 +11,44 @@ public class GroceryList {
     public void addGroceryItem(String item) {
         groceryList.add(item);
     }
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your list");
         for (int i=0; i<groceryList.size(); i++) {
             System.out.println((i+1) + ". " + groceryList.get(i));
         }
     }
-    public void modifyGroceryItem(int position, String newItem) {
+    public void modifyGroceryItem(String currentItem, String newItem){
+        int position = findItem(currentItem);
+        if(position >= 0) {
+            modifyGroceryItem(position, newItem);
+        }
+    }
+    private void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
         System.out.println("Grocery item " + (position+1) + " has been modified");
     }
-    public void removeGroceryItem(int position) {
-        String theItem = groceryList.get(position);
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if(position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+    private void removeGroceryItem(int position) {
         groceryList.remove(position);
     }
-    public String findItem(String searchItem) {
-        boolean exists = groceryList.contains(searchItem);
-        if (exists) {
-            int position = groceryList.indexOf(searchItem);
-            return groceryList.get(position);
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        if(position >=0) {
+            return true;
         }
-        return null;
+        return false;
+    }
+    private int findItem(String searchItem) {
+       return groceryList.indexOf(searchItem);
     }
 }
