ac580afebe0471d3455cc124188651c9ec29f867
Vygis <vygis83@gmail.com>
2015-12-31 17:34:38 +0000

    Linked list challenge

---
+++ LinkedListChallenge/src/com/vygutis/Song.java
@@ -0,0 +1,23 @@
+package com.vygutis;
+
+/**
+ * Created by Vygis on 31/12/2015.
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
