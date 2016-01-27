65198910a7e3dc38173b959f8d6342db3c5b4ff4
Vygis <vygis83@gmail.com>
2016-01-27 16:25:44 +0000

    Adventure Challenge

--- Adventure/src/com/vygutis/Main.java
+++ Adventure/src/com/vygutis/Main.java
@@ -9,7 +9,7 @@ public class Main {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
     public static void main(String[] args) {
-	// write your code here
+
         Scanner scanner = new Scanner(System.in);
 
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
@@ -19,6 +19,28 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+        Map<String, String> pairs = new HashMap<String, String>();
+        pairs.put("NORTH", "N");
+        pairs.put("SOUTH", "S");
+        pairs.put("WEST", "W");
+        pairs.put("EAST", "E");
+        pairs.put("QUIT", "Q");
+
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
@@ -26,11 +48,34 @@ public class Main {
                 break;
             }
 
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)) {
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] sentence = direction.split(" ");
+                for (String i : sentence){
+                    if (pairs.containsKey(i)) {
+                        direction = pairs.get(i);
+                        break;
+                    }
+                }
+            }
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
+
+//        String[] road = "You are standing at the end of the road before a small brick building".split(" ");
+//        for (String i : road) {
+//            System.out.println(i);
+//        }
 
     }
 }
